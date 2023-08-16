12abf861b67c3437403b4a35047c0f8787e99b86
kungchan <135184718+kungchan1111@users.noreply.github.com>
2023-08-16 10:44:33 +0900

    9차 완

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skills.css";
 import { skillsText } from "./data1";
+import animation from "../assets/img/animation.gif";
 
 export default function Skills() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <section id="skills" className="skills S__sticky">
       <div className="S__container">
@@ -11,7 +22,12 @@ export default function Skills() {
             <span>My skills</span>
           </div>
           {skillsText.map((skills, index) => (
-            <div key={index} className="S__logos">
+            <div
+              key={index}
+              className={`S__logos ${isHovered ? "hovered" : ""}`}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
               <img src={skills.img} alt="이미지를 불러오는중" />
               <span className="S__text">{skills.desc}</span>
             </div>
@@ -28,6 +44,14 @@ export default function Skills() {
               </span>
             </div>
           ))}
+          <div className={`S__background ${isHovered ? "hidden" : ""}`}>
+            <img
+              src={animation}
+              alt="이미지를 불러오는중"
+              className="gif__background"
+            />
+            <span>아이콘 위에 마우스를 올려보세요!</span>
+          </div>
         </div>
       </div>
     </section>
